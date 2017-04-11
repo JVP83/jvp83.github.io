decfc5e5e7cd6c0359d9755cf2b3d606c70b226b
JVP83 <jiuliuspuscas@icloud.com>
2017-04-11 18:22:16 +1000

    updated files
    updated files

--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,29 @@
+if ($('.the-slider').length){ 
+		$('.the-slider').slick({
+			dots: true,
+			autoplay: true,
+			autoplaySpeed: 3000
+		});
+};
+
+if($('.parallax').length){
+		$('.pax-1').parallax({imageSrc: 'images/white_family.jpg'});
+
+		// $('.pax-2').parallax({imageSrc: 'http://placeimg.com/1600/1000/nature/2'});
+
+		$('.pax-3').parallax({imageSrc: 'images/wm_about-us_phase2c_05.jpg'});
+
+		$('.pax-4').parallax({imageSrc: 'images/white_family.jpg'});
+
+		$('.pax-5').parallax({imageSrc: 'images/wm_about-us_phase2c_05.jpg'});
+
+}
 
 
-
-
-$('.the-slider').slick({
-	dots: true,
-	autoplay: false,
-});
-
+// $("nav").find("a").click(function(e) {
+//    e.preventDefault();
+//    var section = $(this).attr("href");
+//    $("html, body").animate({
+//        scrollTop: $(section).offset().top
+//    });
+// });
